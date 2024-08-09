b821d8fe4be9a47048833fffaa4eaf93d0dcf6c8
Cristian <dcris3344@gmail.com>
2024-08-09 16:00:22 -0500

    color de menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 let boton = document.getElementById('burguer')
-// let contNav = document.getElementByClassName('navbar');
-// let menu = document.getElementById('nav-menu');
 
 boton.addEventListener('click', function() {
     boton.classList.toggle('is-active');
     document.getElementById('nav-menu').classList.toggle('activo')
+    document.getElementById('header').classList.toggle('activador')
 });
