d579874d3d3bb5a2116e0e279dca028c27081010
Cristian <dcris3344@gmail.com>
2024-08-09 15:40:07 -0500

    menu adaptable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let boton = document.getElementById('burguer')
+// let contNav = document.getElementByClassName('navbar');
+// let menu = document.getElementById('nav-menu');
 
 boton.addEventListener('click', function() {
     boton.classList.toggle('is-active');
+    document.getElementById('nav-menu').classList.toggle('activo')
 });
